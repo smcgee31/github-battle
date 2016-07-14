516766c1d0bdb43e5faf61c61859589bdf0e45cc
smcgee31 <solodesign31@gmail.com>
2016-07-14 16:07:51 -0600

    progress through video 8

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,14 +1,49 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function puke(obj) {
   return <pre>{JSON.stringify(obj, null, ' ')}</pre>
 }
 
+
 // Stateless Functional Component
+// notice the Conditional operator that asks if props.isLoading true then render LOADING, otherwise render the rest
 function ConfirmBattle(props) {
   return props.isLoading === true
     ? <p> LOADING! </p>
-    : <div> CONFIRM BATTLE!: {puke(props)} </div>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <UserDetailsWrapper header='Player One'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player Two'>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            <button className="btn btn-lg btn-success" type="button" onClick={props.onInitiateBattle}>
+              Initiate Battle!
+            </button>
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            <Link to='/playerOne'>
+              <button className="btn btn-lg btn-danger" type="button">Reselect Players</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ var Home = React.createClass ({
     return (
       <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
         <h1>Github Battle</h1>
-        <p className="lead">Some Fancy Motto</p>
+        <p className="lead">Let's get it ON!</p>
         <Link to='/playerOne'>
           <button className="btn btn-lg btn-success" type="button">Get Started</button>
         </Link>
